2303cf800f89c82e2f0c0858f95ea74785f454b2
vivekpanigrahy <vivek.panigrahy76@gmail.com>
2019-11-23 21:06:34 -0700

    Final Upload pre-process.py for all datasets

--- pre-process.py
+++ pre-process.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 import nltk
 from nltk.corpus import stopwords 
-from nltk.tokenize import word_tokenize
-from difflib import SequenceMatcher
+import csv
+
 
 stop_words = set(stopwords.words('english'))
 
@@ -18,15 +18,28 @@ df_train = df_train.dropna(subset =['label'])
 # print("Training set :")
 # print(df_train.head(10))
 
-# df_val = pd.read_csv("./data/validation.csv")
-# df_val = df_val.drop('Unnamed: 6', axis=1)
-# # print("Validation set :")
-# # print(df_val.head(10))
+df_val = pd.read_csv("./data/validation.csv")
+df_val = df_val.drop('Unnamed: 6', axis=1)
+df_val = df_val.dropna(subset =['id'])
+df_val = df_val.dropna(subset =['tid1'])
+df_val = df_val.dropna(subset =['tid2'])
+df_val = df_val.dropna(subset =['title1_en'])
+df_val = df_val.dropna(subset =['title2_en'])
+df_val = df_val.dropna(subset =['label'])
+# print("Validation set :")
+# print(df_val.head(10))
 
-# df_test = pd.read_csv("./data/test.csv")
-# df_test = df_test.drop(df_test.ix[:,'Unnamed: 5':'Unnamed: 6'].head(0).columns, axis=1)
-# # print("Test set :")
-# # print(df_test.head(10))
+df_test = pd.read_csv("./data/test.csv")
+df_test = df_test.drop(df_test.ix[:,'Unnamed: 5':'Unnamed: 6'].head(0).columns, axis=1)
+df_test = df_test.dropna(subset =['id'])
+df_test = df_test.dropna(subset =['tid1'])
+df_test = df_test.dropna(subset =['tid2'])
+df_test = df_test.dropna(subset =['title1_en'])
+df_test = df_test.dropna(subset =['title2_en'])
+# print("Test set :")
+# print(df_test.head(10))
+
+# IMPORTANT :  CHANGE DATAFRAME VALUES ACCORDINGLY FROM HERE TO ENSURE PRE-PROCESSING HAPPENS PROPERLY
 
 temp_en = []
 temp_title = []
@@ -34,8 +47,8 @@ temp_title = []
 for each_row in range(len(df_train)):
     temp_en.append(df_train.iloc[each_row][3])
     temp_title.append(df_train.iloc[each_row][4])
-print(temp_en[0:10])
-print(temp_title[0:10])            
+# print(temp_en[0:10])
+# print(temp_title[0:10])            
 temp =[]
 temp1 =[]
 for i in range(len(temp_en)):
@@ -43,8 +56,8 @@ for i in range(len(temp_en)):
     each_sentence_title = temp_title[i]
 
     if len(each_sentence) >0:
-        sen_l =each_sentence.split()
-        sen_l_title = each_sentence_title.split()
+        sen_l =each_sentence.lower().split()
+        sen_l_title = each_sentence_title.lower().split()
         s=""
         r=""
         for word in sen_l:
@@ -64,3 +77,17 @@ for i in range(len(temp_en)):
         continue
 # print(temp_en[0:10])
 # print(temp_title[0:10])
+id_1=[]
+tid1=[]
+tid2=[]
+label=[] #DELETE LABEL IF YOU WANT TO PR-PROCESS TEST.CSV
+for i in range(len(df_train)):
+    id_1.append(df_train.iloc[i][0])
+    tid1.append(df_train.iloc[i][1])
+    tid2.append(df_train.iloc[i][2])
+    label.append(df_train.iloc[i][5]) #DELETE LABEL IF YOU WANT TO PR-PROCESS TEST.CSV
+
+df = pd.DataFrame(list(zip(*[id_1,tid1,tid2,temp_en,temp_title,label]))).add_prefix('Col') #DELETE LABEL IF YOU WANT TO PR-PROCESS TEST.CSV
+df.columns = ['id','tid1','tid2','Fake_Title','Title','Label'] #DELETE LABEL IF YOU WANT TO PR-PROCESS TEST.CSV
+df.to_csv('./data/validation_mod.csv', index=False)
+print("Check the modified training file")
