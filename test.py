5f5c44768a43c3248c52ff3bfd277d2ade1e6870
vivekpanigrahy <vivek.panigrahy76@gmail.com>
2019-11-26 20:23:35 -0700

    partially working prediction script

--- test.py
+++ test.py
@@ -25,8 +25,18 @@ label_train = []
 for each_row in range(len(df_train)):
     fake_title_train.append(df_train.iloc[each_row][3])
     title_train.append(df_train.iloc[each_row][4])
-    label_train.append(df_train.iloc[each_row][5])
+    if df_train.iloc[each_row][5] == 'unrelated':
+        label_train.append(0)
+        continue
+    elif df_train.iloc[each_row][5] == 'agreed':
+        label_train.append(1)
+        continue
+    else:
+        label_train.append(2)
+    
+    # label_train.append(df_train.iloc[each_row][5])
 print("Training Lists created")
+print(label_train)
 
 similarity_train = []
 
@@ -50,15 +60,24 @@ label = []
 for each in range(len(df_val)):
     fake_title.append(df_val.iloc[each][3])
     title.append(df_val.iloc[each][4])
-    label.append(df_val.iloc[each][5])
+    if df_val.iloc[each][5] == 'unrelated':
+        label.append(0)
+        continue
+    elif df_val.iloc[each][5] == 'agreed':
+        label.append(1)
+        continue
+    else:
+        label.append(2)
+    # label.append(df_val.iloc[each][5])
 print("Validation Lists created")
+print(label)
 
 similarity_val = []
 for each_element in range(len(fake_title)):
     counterA = Counter(fake_title[each_element])
     counterB = Counter(title[each_element])
     similarity_val.append(counter_cosine_similarity(counterA, counterB))
-print("Similarity values for training calculated")
+print("Similarity values for validation calculated")
 print(similarity_val[:10])
 
 x_train, y_train, x_test, y_test = similarity_train, label_train, similarity_val, label
@@ -66,11 +85,15 @@ x_train, y_train, x_test, y_test = similarity_train, label_train, similarity_val
 regressor = LinearRegression()
 x_train = np.array(x_train).reshape((-1, 1))
 y_train = np.array(y_train).reshape(-1,1)
+
+x_test = np.array(x_test).reshape((-1, 1))
+y_test = np.array(y_test).reshape(-1,1)
+
 regressor.fit(x_train, y_train)
 
 y_pred = regressor.predict(x_test)
-df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
-print(df.head(10))
+df = pd.DataFrame([{'Actual': y_test, 'Predicted': y_pred}])
+print(df)
 
 # print("Training set :")
 # print(df_train.head(10))
